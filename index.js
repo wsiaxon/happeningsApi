890c7a0da999f66f4f008131d78ac6335293fe56
Kufre Godwin <kufy201@gmail.com>
2020-08-19 13:30:02 +0100

    home route added

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get("/", function (req, res) {
   res.send("Hello World! It's Happening!");
 });
 
+app.get("/home", function (req, res) {
+  res.send("this is my home now!");
+});
+
 const port = process.env.PORT || 8080;
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
